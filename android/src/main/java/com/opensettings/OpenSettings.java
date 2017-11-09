4c468e299211a91057024e08295a10c105537bbf
Bence Zsigmond Nagy <bence@MacBook-Pro.local>
2017-11-09 09:26:22 +0100

    Now opens general settings instead of app settings

--- android/src/main/java/com/opensettings/OpenSettings.java
+++ android/src/main/java/com/opensettings/OpenSettings.java
@@ -26,14 +26,7 @@ public class OpenSettings extends ReactContextBaseJavaModule {
     //region React Native Methods
     @ReactMethod
     public void openSettings() {
-        final Intent i = new Intent();
-        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setData(Uri.parse("package:" + reactContext.getPackageName()));
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        reactContext.startActivity(i);
+        reactContext.startActivity(new Intent(Settings.ACTION_SETTINGS));
     }
     //endregion
 }
